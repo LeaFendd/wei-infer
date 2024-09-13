a27b3e7ae98756b208064f19f693e2da6ca43413
wding <weiding@stu.xidian.edu.cn>
2024-09-13 11:04:38 +0800

    测试函数

---
+++ test/utils.cuh
@@ -0,0 +1,6 @@
+#ifndef TEST_CU_CUH
+#define TEST_CU_CUH
+void test_function(float *arr, int size, float value = 1.f);
+
+void set_value_cu(float *arr_cu, int size, float value = 1.f);
+#endif // TEST_CU_CUH
