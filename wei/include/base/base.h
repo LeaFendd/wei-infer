1235056c7b29273e7ebfa64146a6ca3f8ca818f6
wding <weiding@stu.xidian.edu.cn>
2024-09-14 10:31:46 +0800

    异常处理

--- wei/include/base/base.h
+++ wei/include/base/base.h
@@ -40,6 +40,84 @@ inline size_t DataTypeSize(DataType data_type) {
 #define MB (1024 * 1024)
 #define GB (1024 * 1024 * 1024)
 
+enum StatusCode : uint8_t {
+    kSuccess = 0,
+    kFunctionUnImplement = 1,
+    kPathNotValid = 2,
+    kModelParseError = 3,
+    kInternalError = 5,
+    kKeyValueHasExist = 6,
+    kInvalidArgument = 7,
+};
+
+class Status {
+  public:
+    Status(int code = StatusCode::kSuccess, std::string err_message = "");
+
+    Status(const Status &other) = default;
+
+    Status &operator=(const Status &other) = default;
+
+    Status &operator=(int code);
+
+    bool operator==(int code) const;
+
+    bool operator!=(int code) const;
+
+    operator int() const;
+
+    operator bool() const;
+
+    int32_t get_err_code() const;
+
+    const std::string &get_err_msg() const;
+
+    void set_err_msg(const std::string &err_msg);
+
+  private:
+    int code_ = StatusCode::kSuccess;
+    std::string message_;
+};
+
+namespace error {
+#define STATUS_CHECK(call)                                                     \
+    do {                                                                       \
+        const base::Status &status = call;                                     \
+        if (!status) {                                                         \
+            const size_t buf_size = 512;                                       \
+            char buf[buf_size];                                                \
+            snprintf(                                                          \
+                buf,                                                           \
+                buf_size - 1,                                                  \
+                "Infer error\n File:%s Line:%d\n Error code:%d\n Error "       \
+                "msg:%s\n",                                                    \
+                __FILE__,                                                      \
+                __LINE__,                                                      \
+                int(status),                                                   \
+                status.get_err_msg().c_str()                                   \
+            );                                                                 \
+            LOG(FATAL) << buf;                                                 \
+        }                                                                      \
+    } while (0)
+
+Status Success(const std::string &err_msg = "");
+
+Status FunctionNotImplement(const std::string &err_msg = "");
+
+Status PathNotValid(const std::string &err_msg = "");
+
+Status ModelParseError(const std::string &err_msg = "");
+
+Status InternalError(const std::string &err_msg = "");
+
+Status KeyHasExits(const std::string &err_msg = "");
+
+Status InvalidArgument(const std::string &err_msg = "");
+
+} // namespace error
+
+std::ostream &operator<<(std::ostream &os, const Status &x);
+
 } // namespace base
 
 #endif // WEI_INFER_BASE_BASE_H_
